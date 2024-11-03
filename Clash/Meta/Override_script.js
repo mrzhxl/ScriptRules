83ec35e5679c2e58ba0e803cc45a6007811d7cd0
mrzhxl <mrzhxl@gmail.com>
2024-11-03 14:13:22 +0800

    update dns

--- Clash/Meta/Override_script.js
+++ Clash/Meta/Override_script.js
@@ -1,4 +1,4 @@
-// 最后更新时间: 2024-11-02
+// 最后更新时间: 2024-11-03
 
 // 规则集通用配置
 const ruleProviderCommon = {
@@ -67,13 +67,8 @@ function main(config) {
     "ipv6": false,
     "enhanced-mode": "fake-ip",
     "fake-ip-range": "198.18.0.1/16",
-    "fake-ip-filter": ["*", "+.lan", "+.local", "+.direct", "+.msftconnecttest.com", "+.msftncsi.com"],
-    "default-nameserver": ["system"],
+    "fake-ip-filter": ["*", "+.lan", "+.local", "+.direct"],
     "nameserver": ["223.5.5.5", "119.29.29.29", "180.184.1.1"],
-    "nameserver-policy": {
-      "geosite:cn": "system",
-      "geosite:gfw,geolocation-!cn": ["quic://223.5.5.5", "quic://223.6.6.6", "https://1.12.12.12/dns-query", "https://120.53.53.53/dns-query"]
-    }
   };
   // 删除 hosts 配置
   if (config.hasOwnProperty('hosts')) {
